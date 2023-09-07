ccdd527ff71453eacc8906d5d7d4661788e10406
unknown <warramat1999@gmail.com>
2023-09-07 09:34:37 +0700

    test

--- user/userList/test.js
+++ user/userList/test.js
@@ -55,46 +55,46 @@ async function userCreate(name, userid, password) {
 }
 
 /***************************************** UPDATE ******************************************************** */
-async function showUserEditBox(_id) {
-  try {
-    const response = await fetch('https://nr-smartcity.onrender.com/User-admin/user/' + _id)
-    if (response.ok) {
-      const user = await response.json()
-      Swal.fire({
-        title: 'แก้ไขข้อมูลผู้ใช้',
-        html:
-          '<input id="userid1" disable value=' + user['userid'] + '>' +
-          '<input id="name1" class="swal2-input"  value="' + user['name'] + '">' +
-          '<input id="password1" class="swal2-input">',
-        focusConfirm: false,
-        preConfirm: () => {
-          const name = document.getElementById("f-name").value;
-          const userid = document.getElementById("f-userid").value;
-          const password = document.getElementById("password").value;
-          userEdit(name, userid, password);
-        }
-      })
-    }
-  } catch (error) {
+// async function showUserEditBox(_id) {
+//   try {
+//     const response = await fetch('https://nr-smartcity.onrender.com/User-admin/user/' + _id)
+//     if (response.ok) {
+//       const user = await response.json()
+//       Swal.fire({
+//         title: 'แก้ไขข้อมูลผู้ใช้',
+//         html:
+//           '<input id="userid1" disable value=' + user['userid'] + '>' +
+//           '<input id="name1" class="swal2-input"  value="' + user['name'] + '">' +
+//           '<input id="password1" class="swal2-input">',
+//         focusConfirm: false,
+//         preConfirm: () => {
+//           const name = document.getElementById("f-name").value;
+//           const userid = document.getElementById("f-userid").value;
+//           const password = document.getElementById("password").value;
+//           userEdit(name, userid, password);
+//         }
+//       })
+//     }
+//   } catch (error) {
 
-  }
-}
+//   }
+// }
 
 
 
-async function userEdit(name, userid, password) {
-  try {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json")
-    await fetch("https://nr-smartcity.onrender.com/User-admin/user/" + userid, {
-      method: "PUT",
-      headers: myHeaders,
-      body: JSON.stringify(password.length > 0 ? { name, password } : { name })
-    })
-    await Swal.fire({ text: "บันทึกข้อมูลเสร็จสิ้น" })
-    loadTable()
-  } catch (error) { }
-}
+// async function userEdit(name, userid, password) {
+//   try {
+//     var myHeaders = new Headers();
+//     myHeaders.append("Content-Type", "application/json")
+//     await fetch("https://nr-smartcity.onrender.com/User-admin/user/" + userid, {
+//       method: "PUT",
+//       headers: myHeaders,
+//       body: JSON.stringify(password.length > 0 ? { name, password } : { name })
+//     })
+//     await Swal.fire({ text: "บันทึกข้อมูลเสร็จสิ้น" })
+//     loadTable()
+//   } catch (error) { }
+// }
 
 
 /***************************************** DELETE *********************************************** */
@@ -122,21 +122,21 @@ async function userDelete(_id) {
 
 
 /******************************************************SEARCH******************************************** */
-async function loadTable(keyword) {
-  try {
-    let response = await fetch("https://nr-api-smartcity-final.onrender.com/User-admin/adminAll/search?keyword="+keyword)
-    if (response.ok) {
-      var trHTML = '';
-      const objects = await response.json()
-      for (let object of objects) {
-        trHTML += '<tr>';
-        trHTML += '<td>' + object['name'] + '</td>';
-        trHTML += '<td>' + object['userid'] + '</td>';
-        trHTML += '<td><button type="button" class="btn btn-outline-secondary" onclick=\'showUserEditBox("' + object['_id'] + '")\'>Edit</button>';
-        trHTML += '<button type="button" class="btn btn-outline-danger" onclick=\'userDelete("' + object['_id'] + '")\'>Del</button></td>';
-        trHTML += "</tr>";
-      }
-      document.getElementById("mytable").innerHTML = trHTML;
-    }
-  } catch (e) { }
-}
+// async function loadTable(keyword) {
+//   try {
+//     let response = await fetch("https://nr-api-smartcity-final.onrender.com/User-admin/adminAll/search?keyword="+keyword)
+//     if (response.ok) {
+//       var trHTML = '';
+//       const objects = await response.json()
+//       for (let object of objects) {
+//         trHTML += '<tr>';
+//         trHTML += '<td>' + object['name'] + '</td>';
+//         trHTML += '<td>' + object['userid'] + '</td>';
+//         trHTML += '<td><button type="button" class="btn btn-outline-secondary" onclick=\'showUserEditBox("' + object['_id'] + '")\'>Edit</button>';
+//         trHTML += '<button type="button" class="btn btn-outline-danger" onclick=\'userDelete("' + object['_id'] + '")\'>Del</button></td>';
+//         trHTML += "</tr>";
+//       }
+//       document.getElementById("mytable").innerHTML = trHTML;
+//     }
+//   } catch (e) { }
+// }
